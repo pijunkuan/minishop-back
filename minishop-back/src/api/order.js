4dfc76acf93ed4d10727dbed446f4bb358ce49e8
pijunkuan@gmail.com <pijunkuan@gmail.com>
2020-05-07 04:00:38 +0800

    订单api

--- minishop-back/src/api/order.js
+++ minishop-back/src/api/order.js
@@ -8,9 +8,33 @@ export function get_orders(data){
 	})
 }
 
-export function get_order(no){
+export function get_order(id){
 	return request({
-		url:'order/'+no,
+		url:'order/'+id,
 		method:'get'
+	})
+}
+
+export function status_update_order(id,data){
+	return request({
+		url:'order/status/' + id,
+		method:'put',
+		data
+	})
+}
+
+export function update_order(id,data){
+	return request({
+		url:'order/' + id,
+		method:"put",
+		data
+	})
+}
+
+export function send_order(id,data){
+	return request({
+		url:'order/'+id+'/shipment',
+		method:"post",
+		data
 	})
 }
